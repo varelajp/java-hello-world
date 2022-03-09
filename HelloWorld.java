f647c83f4fe93d285b89a9d6d246cf9edb50a0b4
Juan Varela <juanpvarela@gmail.com>
2022-03-08 22:17:10 -0400

    Add initial set of files

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * Says Hello! to World.
+ */
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    byeWorld();
+  }
+
+  public static void byeWorld() {
+    System.out.println("Bye, World");
+  }
+}
+
+// public class HelloWorld2 {
+//   static {
+//     System.out.println("Hello, World 2");
+//     System.exit(0); // prevents “main method not found” error
+//   }
+// }
